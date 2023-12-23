61bfabdbcda1ddfd6649e6dbf404e29d8ecd403c
Robert <Robert@DESKTOP-AN7LPIU>
2023-12-23 20:29:18 +0000

    zadanie domowe 5

--- FirstApp/FirstApp/Program.cs
+++ FirstApp/FirstApp/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Robert Jestem");
+﻿int[] grades = new int[365];
+
+List<string> dayOfWeeks = new List<string>();
+dayOfWeeks.Add("Poniedzialek");
+dayOfWeeks.Add("Wtorek");
+dayOfWeeks.Add("Sroda");
+dayOfWeeks.Add("Czwartek");
+dayOfWeeks.Add("Piatek");
+dayOfWeeks.Add("Sobota");
+dayOfWeeks.Add("Niedziela");
+
+
+foreach(var day in dayOfWeeks)
+{
+    Console.WriteLine(day);
+}
