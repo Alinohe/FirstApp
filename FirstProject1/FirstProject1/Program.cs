7a55d6e52442aa770b7cf38af67484fe76d93038
Robert <Robert@DESKTOP-AN7LPIU>
2023-12-24 14:45:59 +0000

    C5 finished

--- FirstProject1/FirstProject1/Program.cs
+++ FirstProject1/FirstProject1/Program.cs
@@ -1,21 +1,20 @@
-﻿using System.ComponentModel.Design;
-
+﻿
 int number = 4566;
 string numberInString = number.ToString();
 char[] letters = numberInString.ToArray();
 Console.WriteLine("Wyniki Dla Liczby " + number);
 
 int[] count = new int[10];
- count[0] = 0;
- count[1] = 0;
- count[2] = 0;
- count[3] = 0;
- count[4] = 0;
- count[5] = 0;
- count[6] = 0;
- count[7] = 0;
- count[8] = 0;
- count[9] = 0;
+count[0] = 0;
+count[1] = 0;
+count[2] = 0;
+count[3] = 0;
+count[4] = 0;
+count[5] = 0;
+count[6] = 0;
+count[7] = 0;
+count[8] = 0;
+count[9] = 0;
 
 foreach (char letter in letters)
 {
@@ -62,16 +61,17 @@ foreach (char letter in letters)
         count[9]++;
     }
 
-    Console.WriteLine("0 =>" + count[0]);
-    Console.WriteLine("1 =>" + count[1]);
-    Console.WriteLine("2 =>" + count[2]);
-    Console.WriteLine("3 =>" + count[3]);
-    Console.WriteLine("4 =>" + count[4]);
-    Console.WriteLine("5 =>" + count[5]);
-    Console.WriteLine("6 =>" + count[6]);
-    Console.WriteLine("7 =>" + count[7]);
-    Console.WriteLine("8 =>" + count[8]);
-    Console.WriteLine("9 =>" + count[9]);
+    
 
 }
+Console.WriteLine("0 =>" + count[0]);
+Console.WriteLine("1 =>" + count[1]);
+Console.WriteLine("2 =>" + count[2]);
+Console.WriteLine("3 =>" + count[3]);
+Console.WriteLine("4 =>" + count[4]);
+Console.WriteLine("5 =>" + count[5]);
+Console.WriteLine("6 =>" + count[6]);
+Console.WriteLine("7 =>" + count[7]);
+Console.WriteLine("8 =>" + count[8]);
+Console.WriteLine("9 =>" + count[9]);
 
