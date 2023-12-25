933de0f74753d83ac98212d18a63b237a02a14aa
Robert <Robert@DESKTOP-AN7LPIU>
2023-12-25 14:16:20 +0000

    day4 finished

--- FirstProject1/FirstProject1/Program.cs
+++ FirstProject1/FirstProject1/Program.cs
@@ -1,77 +1,23 @@
 ﻿
-int number = 4566;
-string numberInString = number.ToString();
-char[] letters = numberInString.ToArray();
-Console.WriteLine("Wyniki Dla Liczby " + number);
+string name = "Robert";
+var age = 50;
+var sex = "mezczyzna";
 
-int[] count = new int[10];
-count[0] = 0;
-count[1] = 0;
-count[2] = 0;
-count[3] = 0;
-count[4] = 0;
-count[5] = 0;
-count[6] = 0;
-count[7] = 0;
-count[8] = 0;
-count[9] = 0;
 
-foreach (char letter in letters)
+if (age > 50 && sex == "mezczyzna")
 {
-    if (letter == '0')
-    {
-        count[0]++;
-
-    }
-    else if (letter == '1')
-    {
-        count[1]++;
-    }
-    else if (letter == '2')
-    {
-        count[2]++;
-    }
-    else if (letter == '3')
-    {
-        count[3]++;
-    }
-    else if (letter == '4')
-    {
-        count[4]++;
-    }
-    else if (letter == '5')
-    {
-        count[5]++;
-    }
-    else if (letter == '6')
-    {
-        count[6]++;
-    }
-    else if (letter == '7')
-    {
-        count[7]++;
-
-    }
-    else if (letter == '8')
-    {
-        count[8]++;
-    }
-    else if (letter == '9')
-    {
-        count[9]++;
-    }
-
-    
-
+    Console.WriteLine("mezczyzna ponizej 50 lat");
 }
-Console.WriteLine("0 =>" + count[0]);
-Console.WriteLine("1 =>" + count[1]);
-Console.WriteLine("2 =>" + count[2]);
-Console.WriteLine("3 =>" + count[3]);
-Console.WriteLine("4 =>" + count[4]);
-Console.WriteLine("5 =>" + count[5]);
-Console.WriteLine("6 =>" + count[6]);
-Console.WriteLine("7 =>" + count[7]);
-Console.WriteLine("8 =>" + count[8]);
-Console.WriteLine("9 =>" + count[9]);
 
+else if (name == "Robert" && age == 50)
+{
+    Console.WriteLine("Robert lat 50");
+}
+else if (sex == "kobieta" && age < 18)
+{
+    Console.WriteLine("niepelnoletnmia Kobieta");
+}
+else if (name == "Ewa" && age < 60 && sex == "kobieta")
+{
+    Console.WriteLine("");
+}
